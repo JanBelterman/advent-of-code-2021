89547fc2fe2b29b53e94cfcd44cb3851d62d15ea
Jan Belterman <=>
2021-12-01 20:13:43 +0100

    Day 1 game 2

---
+++ day1-2.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+fs.readFile('input.txt', 'utf-8', function(err, data) {
+    data = data.split('\n')
+    data = data.map(x => parseInt(x))
+
+    console.log(inputToOutput(data))
+})
+
+function inputToOutput(input) {
+    var output = 0
+    var prev = undefined
+
+    for (var i = 0; i < input.length; i += 1) {
+        var window = input[i] + input[i+1] + input[i+2];
+
+        if (prev !== undefined && window > prev) {
+            output += 1
+        }
+        prev = window
+    }
+
+    return output
+}
